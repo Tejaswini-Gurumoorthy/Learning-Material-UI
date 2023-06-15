bdb5d109dec6f53e63ad7a2e6bf2820a1d0db825
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2023-06-15 14:50:31 +0530

    basic design

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,30 @@
-import { Container, ThemeProvider, Typography } from '@material-ui/core'
-import { CssBaseline } from '@mui/material'
-import React from 'react'
+import { Container, ThemeProvider, Typography } from '@material-ui/core';
+import { CssBaseline } from '@mui/material';
+import React from 'react';
 import useStyles from '../styles';
 import primaryMainTheme from '../theme';
+import Vector from '../assets/Vector.png';
+import Ellipse1 from '../assets/Ellipse1.png';
+import Ellipse2 from '../assets/Ellipse2.png';
+import Ellipse3 from '../assets/Ellipse3.png';
+
 
 function LoginPage() {
-    const classes= useStyles();
+  const classes = useStyles();
+
   return (
     <>
-    <ThemeProvider theme={primaryMainTheme}>
-    <CssBaseline/>
+      <ThemeProvider theme={primaryMainTheme}>
+        <CssBaseline />
         <div className={classes.main}>
-
+          <img src={Vector} alt="Vector" className={classes.vectorImage} />
+          <img src={Ellipse1} alt="Ellipse1" className={classes.ellipse1}/>
+          <img src={Ellipse2} alt="Ellipse2" className={classes.ellipse2}/>
+          <img src={Ellipse3} alt="Ellipse3" className={classes.ellipse3}/>
         </div>
-    </ThemeProvider>
+      </ThemeProvider>
     </>
-    )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,42 @@
-import { makeStyles } from '@material-ui/styles';
-import Background from './assets/BG.png';
 
-const useStyles= makeStyles((theme)=>({
-    root: {
-        backgroundColor: '#2148c0',
-    },
-    main: {
-        minWidth: '100%' ,
-        minHeight: '100vh',
-        backgroundColor: '#2148c0',
-        backgroundImage: `url(${Background})`,
-        backgroundRepeat: 'no-repeat',
-    }}));
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: '#2148c0',
+  },
+  main: {
+    position: 'relative',
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  vectorImage: {
+    height: '100vh',
+    width: 'auto',
+    zIndex: -1,
+  },
+  ellipse1: {
+    width: 'auto',
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0, // to position the image at the bottom
+    left: 0, // to position the image at the left
+  },
+  ellipse2: {
+    width: 'auto',
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0, 
+    left: 0, 
+  },
+  ellipse3: {
+    width: 'auto',
+    zIndex: 1,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+}));
 
 export default useStyles;
